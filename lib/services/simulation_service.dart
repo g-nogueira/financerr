e9e14fad71aed22a9c8f2727728e7fe29112922f
Gustavo Nogueira <gnogueira@letsgetchecked.com>
2025-01-12 19:39:01 +0000

    fix:  simulation service not calculating correctly

--- lib/services/simulation_service.dart
+++ lib/services/simulation_service.dart
@@ -8,26 +8,39 @@ class SimulationService {
     Map<int, double> dailyBalances = {};
 
     // Initialize daily balances for each day of the month
-    for (int day = 1; day <= 31; day++) {
+    for (int day = 0; day <= 31; day++) {
       dailyBalances[day] = 0.0;
     }
 
-    // Process incomes
-    for (var income in incomes) {
-      for (int day = 1; day <= 31; day++) {
-        if (day >= income.incomeDay) {
-          dailyBalances[day] = (dailyBalances[day] ?? 0) + income.value;
-        }
-      }
-    }
+    // Calculate the initial balance (day 0) based on the total income
+    double initialBalance = incomes.fold(0.0, (sum, income) => sum + income.value);
+    dailyBalances[0] = initialBalance;
 
-    // Process expenses
-    for (var expense in expenses) {
-      for (int day = 1; day <= 31; day++) {
-        if (day >= expense.expenseDayOfTheMonth) {
-          dailyBalances[day] = (dailyBalances[day] ?? 0) - expense.valuePlanned;
-        }
+    // Process each day of the month, starting from day 1
+    for (int day = 1; day <= 31; day++) {
+      // Carry over the balance from the previous day
+      dailyBalances[day] = dailyBalances[day - 1] as double;
+
+      if (day < incomes[0].incomeDay) {
+        // If the current day is before the first income day, skip it
+        continue;
       }
+
+      // Sum the expenses for the current day
+      var totalExpense = expenses
+          .where((expense) => expense.expenseDayOfTheMonth == day)
+          .fold(0.0, (sum, expense) => sum + expense.valuePlanned);
+
+      // Subtract expenses for the current day
+      dailyBalances[day] = (dailyBalances[day] ?? 0.0) - totalExpense;
+
+      // Sum the incomes for the current day
+      var totalIncomes = incomes
+          .where((income) => income.incomeDay == day)
+          .fold(0.0, (sum, income) => sum + income.value);
+      
+      // Add incomes for the current day
+      dailyBalances[day] = (dailyBalances[day] ?? 0.0) + totalIncomes;
     }
 
     // Convert the map to a list of daily summaries
